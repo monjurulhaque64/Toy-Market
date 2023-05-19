41ec8f8c026dfeb98d73fc69d73b701259e6a64e
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-05-20 04:12:55 +0600

    banner section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../../Shared/Navbar/Navbar';
 import Footer from '../../Shared/Footer/Footer';
 import Gallery from '../Gallery/Gallery';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <Navbar></Navbar>
+            <Banner></Banner>
             <Gallery></Gallery>
             <Footer></Footer>
         </div>
